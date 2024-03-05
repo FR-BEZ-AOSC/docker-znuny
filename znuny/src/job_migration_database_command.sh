a4c6f0d2f7cf9d8129ea6a57cdd399a114e5c519
mln-nicolas-b <nb@mylittlenest.fr>
2024-03-05 20:13:17 +0100

    Fix initialization SQL scripts

--- znuny/src/job_migration_database_command.sh
+++ znuny/src/job_migration_database_command.sh
@@ -15,21 +15,53 @@ zcli check modules
 zcli check config
 
 export PGPASSWORD=${args[-w]}
-pg_dump -U ${args[-u]} -h ${args[-h]} -p ${args[-p]} -d ${args[-n]} -f ${DUMP_FILE}
 
 sleep 1
 
+pg_dump -U ${args[-u]} -h ${args[-h]} -p ${args[-p]} -d ${args[-n]} -f ${DUMP_FILE}
+# pg_dump -U ${args[-u]} -h ${args[-h]} -p ${args[-p]} -d ${args[-n]} -f ${DUMP_FILE} 2>&1 | \
+#   while true; do
+#     if IFS= read -r MESSAGE; then
+#       if [[ -n "${MESSAGE}" ]]; then
+#         echo -e "{\"timestamp\":\"$(date +'%Y-%m-%d %H:%M:%S')\", \"source\":\"znuny\", \"message\":\"${MESSAGE}\"}"
+#       fi
+#     fi
+#   done &
+
 database_deletion_pgsql "${ZNUNY_DATABASE_HOST}" "${ZNUNY_DATABASE_PORT}" "${ZNUNY_DATABASE_NAME}" "${ZNUNY_DATABASE_USER}" "${ZNUNY_DATABASE_PASSWORD}"
+# database_deletion_pgsql "${ZNUNY_DATABASE_HOST}" "${ZNUNY_DATABASE_PORT}" "${ZNUNY_DATABASE_NAME}" "${ZNUNY_DATABASE_USER}" "${ZNUNY_DATABASE_PASSWORD}" 2>&1 | \
+#   while true; do
+#     if IFS= read -r MESSAGE; then
+#       if [[ -n "${MESSAGE}" ]]; then
+#         echo -e "{\"timestamp\":\"$(date +'%Y-%m-%d %H:%M:%S')\", \"source\":\"znuny\", \"message\":\"${MESSAGE}\"}"
+#       fi
+#     fi
+#   done &
 
 sleep 1
 
 database_role_pgsql "${ZNUNY_DATABASE_HOST}" "${ZNUNY_DATABASE_PORT}" "${ZNUNY_DATABASE_NAME}" "${ZNUNY_DATABASE_USER}" "${ZNUNY_DATABASE_PASSWORD}"
+# database_role_pgsql "${ZNUNY_DATABASE_HOST}" "${ZNUNY_DATABASE_PORT}" "${ZNUNY_DATABASE_NAME}" "${ZNUNY_DATABASE_USER}" "${ZNUNY_DATABASE_PASSWORD}" 2>&1 | \
+#   while true; do
+#     if IFS= read -r MESSAGE; then
+#       if [[ -n "${MESSAGE}" ]]; then
+#         echo -e "{\"timestamp\":\"$(date +'%Y-%m-%d %H:%M:%S')\", \"source\":\"znuny\", \"message\":\"${MESSAGE}\"}"
+#       fi
+#     fi
+#   done &
 
 sleep 1
 
 export PGPASSWORD=${ZNUNY_DATABASE_PASSWORD}
 
-# echo "PGPASSWORD=${ZNUNY_DATABASE_PASSWORD} pg_restore -U ${ZNUNY_DATABASE_USER} -h ${ZNUNY_DATABASE_HOST} -p ${ZNUNY_DATABASE_PORT} -d ${ZNUNY_DATABASE_NAME} ${DUMP_FILE}"
 psql -U ${ZNUNY_DATABASE_USER} -h ${ZNUNY_DATABASE_HOST} -p ${ZNUNY_DATABASE_PORT} -d ${ZNUNY_DATABASE_NAME} < ${DUMP_FILE}
+# psql -U ${ZNUNY_DATABASE_USER} -h ${ZNUNY_DATABASE_HOST} -p ${ZNUNY_DATABASE_PORT} -d ${ZNUNY_DATABASE_NAME} < ${DUMP_FILE} 2>&1 | \
+#   while true; do
+#     if IFS= read -r MESSAGE; then
+#       if [[ -n "${MESSAGE}" ]]; then
+#         echo -e "{\"timestamp\":\"$(date +'%Y-%m-%d %H:%M:%S')\", \"source\":\"znuny\", \"message\":\"${MESSAGE}\"}"
+#       fi
+#     fi
+#   done &
 
 rm -f ${DUMP_FILE}
