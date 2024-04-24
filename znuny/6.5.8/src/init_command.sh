94d5798d467478d944fb5155494ea31a333ded63
mln-nicolas-b <nb@mylittlenest.fr>
2024-04-24 23:38:38 +0200

    New version: 6.5.8

---
+++ znuny/6.5.8/src/init_command.sh
@@ -0,0 +1,30 @@
+zcli locales
+
+zcli user system
+
+zcli config znuny
+
+zcli config apache
+
+zcli config crons
+
+zcli user permissions
+
+zcli config database
+
+zcli user admin
+
+zcli check modules
+
+zcli check config
+
+zcli config modules
+
+zcli upgrade
+
+zcli run cron &
+
+zcli run daemon &
+
+zcli run apache2
+
